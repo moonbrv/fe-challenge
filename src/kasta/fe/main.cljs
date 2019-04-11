eb683ce10062ec84bf1d35867ba193d5d0bee798
Dmitry Palamarchuk <moonbrv@gmail.com>
2019-04-12 01:18:08 +0300

    add loading and error status for fetching data

--- src/kasta/fe/main.cljs
+++ src/kasta/fe/main.cljs
@@ -34,21 +34,30 @@
                 (not (empty? (clojure.set/intersection filter-set tags-set))))) campaigns-list)))
 
 ;; ----- MODEL -----
-(defonce state (atom {:campaigns         []
-                      :active-tags       #{}
-                      :available-filters []}))
+(defonce state (atom {:campaigns          []
+                      :active-tags        #{}
+                      :available-filters  []
+                      :loading            false
+                      :error              false}))
 
 (def campaigns (rum/cursor-in state [:campaigns]))
 (def active-filters (rum/cursor-in state [:active-tags]))
 (def campaigns-filters (rum/cursor-in state [:available-filters]))
 
+(defn merge-state [state-map]
+  (swap! state merge state-map))
+
 (defn fetch-campaigns []
+  (merge-state {:loading true})
   (go (let [response (<! (http/get "/api/campaigns"))
             data (:body response)
             active-campaigns (filter campaign-active? (:items data))
             available-filters (get-active-campaigns-filters active-campaigns (:menu data))]
-        (swap! state merge {:campaigns         active-campaigns
-                            :available-filters available-filters}))))
+        (merge-state {:loading false})
+        (if (= :no-error (:error-code response))
+          (merge-state {:campaigns         active-campaigns
+                        :available-filters available-filters})
+          (merge-state {:error true})))))
 
 (defn filters-has-intersection [tags filters]
   (let [tag-set (set tags)]
@@ -78,20 +87,30 @@
       (let [all-active-filters (rum/react active-filters)
             tag (:tag campaign-filter)
             is-active-filter (filters-has-intersection tag all-active-filters)]
-        [:div.column {:key (:name campaign-filter)} [:button.button {:on-click (fn []
-                                                  (if is-active-filter
-                                                    (swap! active-filters #(reduce disj % tag))
-                                                    (swap! active-filters #(reduce conj % tag))))
-                                      :class    (if is-active-filter "is-primary" nil)}
-                      (:name campaign-filter)]]))]])
+        [:div.column {:key (:name campaign-filter)}
+         [:button.button {:on-click (fn []
+                                      (if is-active-filter
+                                        (swap! active-filters #(reduce disj % tag))
+                                        (swap! active-filters #(reduce conj % tag))))
+                          :class    (if is-active-filter "is-primary" nil)}
+          (:name campaign-filter)]]))]])
 
 (rum/defc Root
   < fetch-campaigns-mixin
     rum/reactive
   []
-  [:section.section
-   [:div.container.has-text-centered (buttons-list)]
-   [:div.container (campaigns-grid (get-matched-campaigns (rum/react campaigns) (rum/react active-filters)))]])
+  (cond
+    (:loading (rum/react state)) [:section.hero.is-primary
+                                  [:div.hero-body
+                                   [:div.container
+                                    [:h1.title "Loading..."]]]]
+    (:error (rum/react state)) [:section.hero.is-danger
+                                [:div.hero-body
+                                 [:div.container
+                                  [:h1.title "An error ocured during fetching data"]]]]
+    :else [:section.section [:div.container
+                             [:div.has-text-centered (buttons-list)]
+                             (campaigns-grid (get-matched-campaigns (rum/react campaigns) (rum/react active-filters)))]]))
 
 
 (defn ^:export trigger-render []
